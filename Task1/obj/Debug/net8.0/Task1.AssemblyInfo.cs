7d11f02d4ff15b0f47573c8aa59aa2b57411c3fb
Daniar <daiarumbetov30@gmail.com>
2024-04-10 21:17:59 +0500

    add some variables

--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0725503f0e5d8c04c5a62b6528a006e271707d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94f1aaeb39eb751bd137d57ccd77fcbcf7bd5295")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
